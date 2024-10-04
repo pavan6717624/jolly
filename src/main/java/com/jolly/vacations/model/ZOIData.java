b22bdbeacbe92faaec5627631cdf8fed603f9512
Pavan <pvn.kumar4@gmail.com>
2024-10-04 16:37:05 +0530

    deply

---
+++ src/main/java/com/jolly/vacations/model/ZOIData.java
@@ -0,0 +1,18 @@
+package com.jolly.vacations.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ZOIData {
+
+	Long price;
+	Long instrument;
+	String call;
+	
+	List<Double> close, open, high, low, oi, vol;
+	List<String> date;
+	
+	
+}
